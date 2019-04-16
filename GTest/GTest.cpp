29fb9e59bfa0530258decf70cb666fdbfc185b53
J.Chen <424247407@qq.com>
2019-04-16 15:25:40 +0800

    google test

---
+++ GTest/GTest.cpp
@@ -0,0 +1,23 @@
+﻿// GTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "gtest/googletest/include/gtest.h"
+
+
+TEST(AAA, a1)
+{
+	EXPECT_EQ(2, 1 + 1);
+}
+TEST(AAA, a2)
+{
+	EXPECT_EQ(2, 1 + 3);
+}
+
+int main(int argc,char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+    std::cout << "Hello World!\n"; 
+	return RUN_ALL_TESTS();
+}
